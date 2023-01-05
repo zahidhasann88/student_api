e402a48952960e37c9e29980a60ce58618e44f19
Zahid Hasan <jahidhasann67@gmail.com>
2023-01-05 17:30:49 +0600

    first commit

---
+++ StudentAPI/Models/StudentDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentAPI.Models
+{
+    public class StudentDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string RollNumber { get; set; } = null!;
+        public string? Country { get; set; }
+    }
+}
